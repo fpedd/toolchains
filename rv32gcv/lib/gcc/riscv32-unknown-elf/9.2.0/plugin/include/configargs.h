78c2ab4b10a7b696c742d4319b77178af34eb9cc
Fabian Peddinghaus <fabianpedd@gmail.com>
2021-11-17 18:18:51 +0100

    rv32gcv toolchain

---
+++ rv32gcv/lib/gcc/riscv32-unknown-elf/9.2.0/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "/tmp/rv/riscv-gnu-toolchain/riscv-gcc/configure --target=riscv32-unknown-elf --prefix=/usr/local/research/projects/SystemDesign/tools/riscv/rv32gcv --disable-shared --disable-threads --enable-languages=c,c++ --with-system-zlib --enable-tls --with-newlib --with-sysroot=/usr/local/research/projects/SystemDesign/tools/riscv/rv32gcv/riscv32-unknown-elf --with-native-system-header-dir=/include --disable-libmudflap --disable-libssp --disable-libquadmath --disable-libgomp --disable-nls --disable-tm-clone-registry --src=.././riscv-gcc --enable-checking=yes --disable-multilib --with-abi=ilp32d --with-arch=rv32gcv --with-tune=rocket 'CFLAGS_FOR_TARGET=-Os  -mcmodel=medlow' 'CXXFLAGS_FOR_TARGET=-Os  -mcmodel=medlow'";
+static const char thread_model[] = "single";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { "abi", "ilp32d" }, { "arch", "rv32gcv" }, { "tune", "rocket" } };
